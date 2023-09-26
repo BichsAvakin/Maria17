04e6355afecd79ced2a57cd5d02d2807e6a5a426
BichsAvakin <105650956+BichsAvakin@users.noreply.github.com>
2023-09-26 11:56:55 -0300

    Add files via upload

---
+++ musica/Musica.php
@@ -0,0 +1,11 @@
+<?php
+
+class Musica{
+    private $titulo;
+    private $artista;
+    private $genero;
+    private $duracao;
+    private $anodelancamento;
+
+    function__contruct(string $titulo,string $artista, string $genero, string $duracao, string $anodelancamento);
+}
